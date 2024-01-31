acad7d6e11a346792960ed366611e9d2a6131ea8
Leonel <leonelfsotopublic@gmail.com>
2024-01-31 09:49:20 -0300

    incorporacion de nuevos enrutadores para las plantillas ejs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import ejs from "ejs";
 import {dirname, join} from "path";
 import { fileURLToPath } from "url";
+import indexRouter from "./routers/index.js"
 
 
 const app = express();
@@ -13,6 +14,4 @@ console.log("Server is listen on port 3000");
 app.set("view engine", "ejs");
 app.set("views", join(__dirname, "views"));
 
-app.get("/", (req, res) => res.render("index"))
-app.get("/about", (req, res) => res.render("about"))
-app.get("/contact", (req, res) => res.render("contact"))
+app.use(indexRouter);
