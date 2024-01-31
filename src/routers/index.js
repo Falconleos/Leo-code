25e070137c833a81e3f6891fc95be53f20fa08c2
Leonel <leonelfsotopublic@gmail.com>
2024-01-31 10:09:31 -0300

    incorporacion de partials de nav y footer

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,8 +2,8 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => res.render("index"));
-router.get("/about", (req, res) => res.render("about"));
-router.get("/contact", (req, res) => res.render("contact"));
+router.get("/", (req, res) => res.render("index", {title: "My first site on backend"}));
+router.get("/about", (req, res) => res.render("about", {title: "About us"}));
+router.get("/contact", (req, res) => res.render("contact", {title: "Contact us"}));
 
 export default router;
